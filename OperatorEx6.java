a8fd6e22e29d1c0708e29b244b0845f32a8adf13
bslkuye <jdh960401@naver.com>
2021-08-24 17:20:48 +0900

    practice

---
+++ OperatorEx6.java
@@ -0,0 +1,18 @@
+
+public class OperatorEx6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b1 = (5 < 4) ? 50 : 40;
+		
+		int b2 = 0;
+		if(5 < 4) {
+			b2 = 50;
+		}else {
+			b2 = 40;
+		}
+		
+		System.out.println(b2);
+	}
+
+}
