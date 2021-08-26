e2210afa214f7de30c2bded4be38beb94c002757
bslkuye <jdh960401@naver.com>
2021-08-26 16:28:29 +0900

    p

--- src/practiceFile/other/fractalSimulation.java
+++ src/practiceFile/other/fractalSimulation.java
@@ -1,5 +1,5 @@
 
-package otherpractice;
+package practiceFile.other;
 
 import java.util.Random;
 
@@ -22,62 +22,84 @@ public class fractalSimulation {
 		Random random = new Random();
 		random.setSeed(System.currentTimeMillis());
 		boolean contact = false;  //픽셀 움직임에 쓰일 접촉 여부 확인용
-		System.out.print(random.nextInt(4));
+		
+//		System.out.print(random.nextInt(4));
 		int pixelSum = 0;
-		for(;pixelSum < gl ; ) { //생성된 픽셀 수 확인
+		for(;pixelSum < 500 ; ) { //생성된 픽셀 수 확인
 			
 			int x = random.nextInt(gl);
 			int y = random.nextInt(gl);
 			int check = (x - 50) * (x - 50) + (y - 50) * (y - 50);
 			
-			if(check > 2500 || grid[x][y] == false) {   //중점에서 일정거리 이상 떨어져 있으면 위치에 픽셀 생성
+			if(check > 25*25 && grid[x][y] == false) {   //중점에서 일정거리 이상 떨어져 있으면 위치에 픽셀 생성
 				pixelSum++; //생성 성공, 픽셀 총합 1증가
 				
 				//시각화
 				
-//				System.out.println(check+x+y);
-				grid[x][y] = true; //위치에 생성
-				for(;contact == true ; ) {  //픽셀의 움직임 시작 접촉할 때까지
+//				System.out.println(check+"c "+x+"x "+y+"y "+pixelSum+"ps ");
 					
-					if(x == 0)
-						x = 1;
-					if(x == gl - 1)
-						x = gl - 2;
-					if(y == 0)
-						y = 1;
-					if(y == gl - 1)
-						y = gl - 2;//픽셀의 위치가 최 외곽일 경우 안으로 한칸
+				while(contact == false) {  //픽셀의 움직임 시작 접촉할 때까지
+//					System.out.println("move");
+					grid[x][y] = true; //위치에 생성
+
+					if(x == 0) {
+						grid[x][y] = false;
+						grid[x+1][y] = true;
+						x++;
+//						System.out.println("move"+x+" "+y);
+					}
+					if(x == gl - 1) {
+						grid[x][y] = false;
+						grid[x-1][y] = true;
+						x--;
+//						System.out.println("move"+x+" "+y);
+					}
+					if(y == 0) {
+						grid[x][y] = false;
+						grid[x][y+1] = true;
+						y++;
+//						System.out.println("move"+x+" "+y);
+					}
+					if(y == gl - 1) {
+						grid[x][y] = false;
+						grid[x][y-1] = true;
+						y--;//픽셀의 위치가 최 외곽일 경우 안으로 한칸
+//						System.out.println("move"+x+" "+y);
+					}
+					
 					
 					if(grid[x + 1][y] == true || grid[x - 1][y] == true || grid[x][y + 1] == true || grid[x][y - 1] == true) {
-						System.out.println(x+y);
+//						System.out.println(x+" "+y);
 						break;   //픽셀이 고정픽셀과 접해있으면 픽셀 고정
 					}
 					int move = random.nextInt(4);
+					
 					switch(move) {
 					case 0: //상 y++
-						System.out.println(move);
+//						System.out.println(move);
 						grid[x][y] = false;
 						grid[x][y+1] = true;
 						y++;
 						break;
 					case 1: //하 y--
-						System.out.println(move);
+//						System.out.println(move);
 						grid[x][y] = false;
 						grid[x][y-1] = true;
 						y--;
 						break;
 					case 2: //좌 x--
-						System.out.println(move);
+//						System.out.println(move);
 						grid[x][y] = false;
 						grid[x-1][y] = true;
 						x--;
 						break;
 					case 3: //우 x++
-						System.out.println(move);
+//						System.out.println(move);
 						grid[x][y] = false;
 						grid[x+1][y] = true;
 						x++;
 					}
+					
 					
 				}
 			}
@@ -85,9 +107,9 @@ public class fractalSimulation {
 		for(int i = 0; i < gl; i++) {
 			for(int j = 0; j < gl; j++) {
 				if(grid[i][j] == true) {
-					System.out.print(" ■ ");
+					System.out.print("■ ");
 				}else {
-					System.out.print("   ");
+					System.out.print("□ ");
 				}
 			}
 			System.out.println("");
