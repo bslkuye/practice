91e499e52436b34599c1fcb221206d6b9e315b48
bslkuye <jdh960401@naver.com>
2021-09-01 17:49:41 +0900

    t

--- src/practiceFile/lv1/helloworld.java
+++ src/practiceFile/lv1/helloworld.java
@@ -3,7 +3,5 @@ package practiceFile.lv1;
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("test");
-		System.out.println("1");
 	}
 }
