27c90b629ac12950ede12f90869f2f58dd4832db
bslkuye <jdh960401@naver.com>
2021-09-03 23:26:47 +0900

    t

---
+++ src/baekjoon/level1/no1008/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.level1.no1008;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a / b);
+	}
+
+}
