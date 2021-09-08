0f3c168ed23881c8e7fed19716c23bddb365573a
bslkuye <jdh960401@naver.com>
2021-09-08 21:12:57 +0900

    t

--- src/baekjoon/level4/no1110/Main.java
+++ src/baekjoon/level4/no1110/Main.java
@@ -10,31 +10,24 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String standard = br.readLine();
 		String result = standard;
-		int cnt = 0;
+		int cycle = 0;
 		
 		while(true) {
 			StringBuilder sbNum = new StringBuilder(result);
 			if(sbNum.length() < 2) sbNum.insert(0, "0");
 			char secondNum = sbNum.charAt(1);
-			StringBuilder addNum = new StringBuilder(Integer.toString(secondNum))
+			StringBuilder addNum = new StringBuilder(Integer.toString((sbNum.charAt(0) - '0') + (secondNum - '0')));
+			StringBuilder tmp = new StringBuilder();
+			if(secondNum != '0') tmp.append(secondNum);
+			tmp.append(addNum.charAt(addNum.length()-1));
+			result = tmp.toString();
+			
+			cycle++;
+			if(standard.equals(result)) break;
 		}
 		
-
-//		while(true) {
-//			StringBuilder sbNum = new StringBuilder(result);
-//			if(sbNum.length()<2) sbNum.insert(0, "0");
-//			char secondNum = sbNum.charAt(1);
-//			StringBuilder addNum = new StringBuilder(Integer.toString((sbNum.charAt(0) - '0') + (secondNum - '0')));
-//			StringBuilder tmp = new StringBuilder();
-//			if(secondNum != '0') tmp.append(secondNum);
-//			tmp.append(addNum.charAt(addNum.length()-1));
-//			result = tmp.toString();
-//			
-//			cnt++;
-//			if(standard.equals(result)) break;
-//		}
 		
-		System.out.println(cnt);
+		System.out.println(cycle);
 		
 		br.close();
 	}
