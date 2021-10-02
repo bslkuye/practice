1f82f005b0c10c7bd5c22b8ef5507ef920f28b78
bslkuye <jdh960401@naver.com>
2021-10-02 23:09:07 +0900

    t

---
+++ src/practiceFile/lv2/methood.java
@@ -0,0 +1,17 @@
+package practiceFile.lv2;
+
+public class methood {
+
+	public int plus (int a, int b) {
+		return a + b;
+	}
+
+	public int plus (int a, int b, int c) {
+		return a - b - c;
+	}
+	
+	public String plus (String a, String b) {
+		return a + b;
+	}
+	
+}
