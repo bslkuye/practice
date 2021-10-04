85d794dec6bf80e57d4ae76e4b2d69abd84181a2
bslkuye <jdh960401@naver.com>
2021-10-05 03:14:20 +0900

    t

---
+++ src/practiceFile/programmers/lv1/lotto/main.java
@@ -0,0 +1,21 @@
+package practiceFile.programmers.lv1.lotto;
+
+
+public class main {
+	
+	class Solution {
+	    public int[] solution(int[] lottos, int[] win_nums) {
+	        int[] answer = {};
+	        return answer;
+	    }
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// lottos_len은 배열 lottos의 길이입니다.
+		// win_nums_len은 배열 win_nums의 길이입니다.
+	}
+	
+
+}
