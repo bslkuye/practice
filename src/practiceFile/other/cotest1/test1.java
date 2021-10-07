c6c76c3892dbd3c48888035fddb4d923e679d501
bslkuye <jdh960401@naver.com>
2021-10-07 19:30:36 +0900

    t

--- src/practiceFile/other/cotest1/test1.java
+++ src/practiceFile/other/cotest1/test1.java
@@ -19,12 +19,22 @@ public class test1 {
 //			System.out.print(st.nextToken());
 //		}
 //		System.out.println();
+		int maxHeat = 0;
+		int nowHeat = 0;
 		int[][] office = new int[(int)N][(int)N];
 		BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
 		int k = Integer.parseInt(br1.readLine()); // ¿ÂÇ³±â Å©±â
-		for(int i = 0; i < k; i++) {
-			for(int j = 0; j < k; j++) {
-//				office[i][j] = st.nextToken();
+		for(int i = 0; i < (int)N; i++) {
+			for(int j = 0; j < (int)N; j++) {
+				office[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		int[] a = new int[(int)N];
+		for(int i = 0; i < (int)N; i++) a[i] = i;
+		
+		for(int i = 0; a[i] < k; i++) {
+			for(int j = 0; a[j] < k; j++) {
+				
 			}
 		}
 		
