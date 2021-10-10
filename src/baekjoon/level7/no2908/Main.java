1d0beaccb79b3b52d1abdc7ac94c1209589e412e
bslkuye <jdh960401@naver.com>
2021-10-10 20:04:04 +0900

    t

--- src/baekjoon/level7/no2908/Main.java
+++ src/baekjoon/level7/no2908/Main.java
@@ -1,21 +1,39 @@
 package baekjoon.level7.no2908;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int s = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		StringBuilder sb = new StringBuilder();
-		String firstNum = st.nextToken();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int first = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int second = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		
+		if(first <= second) System.out.println(second);
+		else System.out.println(first); 
+		
+		br.close();
+	}
+
+}
+
+//public class Main {
+//
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int s = Integer.parseInt(br.readLine());
+//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//		StringBuilder sb = new StringBuilder();
+//		String firstNum = st.nextToken();
 //		String secondNum = st.nextToken();
-		String changeNum = "";
-		int firstLength = firstNum.length();
+//		String changeNum = "";
+//		int firstLength = firstNum.length();
 //
 //		for(int i = 0; i < firstNum.length(); i++) {
 //			changeNum += firstNum(firstLength-i);
@@ -34,8 +52,8 @@ public class Main {
 //			}
 //			System.out.println(sb);
 //		}
-				
-		br.close();
-	}
-
-}
+//				
+//		br.close();
+//	}
+//
+//}
