d262673b0bd5aaaf2adefa71af3a84dc52b5f388
bslkuye <jdh960401@naver.com>
2021-10-15 15:37:48 +0900

    t

---
+++ src/baekjoon/level7/no1316/Main.java
@@ -0,0 +1,42 @@
+package baekjoon.level7.no1316;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int loop = Integer.parseInt(br.readLine());
+		
+		
+		int result = 0;
+		for(int i=0;i<loop;i++) {
+			String str = br.readLine();
+			HashMap<Character, Boolean> map = new HashMap<>();
+			
+			char tmp = '-';
+			boolean isGroup = true;
+			for(int j=0;j<str.length();j++) {
+				char ch = str.charAt(j);
+				map.putIfAbsent(ch, true);
+				if(tmp != ch) {
+					map.put(tmp, false);
+					if(!map.get(ch)) {
+						isGroup = false;
+						break;
+					}
+				}
+				tmp = ch;
+			}
+			if(isGroup) result++;
+		}
+		
+		System.out.println(result);
+		
+		br.close();
+	}
+
+}
