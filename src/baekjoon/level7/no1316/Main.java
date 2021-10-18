9f94cb83c2d02b14f31de4aa8c116822e2f000b5
bslkuye <jdh960401@naver.com>
2021-10-18 22:07:17 +0900

    t

--- src/baekjoon/level7/no1316/Main.java
+++ src/baekjoon/level7/no1316/Main.java
@@ -8,35 +8,34 @@ import java.util.HashMap;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int loop = Integer.parseInt(br.readLine());
-		
-		
-		int result = 0;
-		for(int i=0;i<loop;i++) {
-			String str = br.readLine();
-			HashMap<Character, Boolean> map = new HashMap<>();
-			
-			char tmp = '-';
-			boolean isGroup = true;
-			for(int j=0;j<str.length();j++) {
-				char ch = str.charAt(j);
-				map.putIfAbsent(ch, true);
-				if(tmp != ch) {
-					map.put(tmp, false);
-					if(!map.get(ch)) {
-						isGroup = false;
-						break;
-					}
-				}
-				tmp = ch;
-			}
-			if(isGroup) result++;
-		}
-		
-		System.out.println(result);
-		
-		br.close();
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int loop = Integer.parseInt(br.readLine());
+//		
+//		int result = 0;
+//		for(int i=0;i<loop;i++) {
+//			String str = br.readLine();
+//			HashMap<Character, Boolean> map = new HashMap<>();
+//			
+//			char tmp = '-';
+//			boolean isGroup = true;
+//			for(int j=0;j<str.length();j++) {
+//				char ch = str.charAt(j);
+//				map.putIfAbsent(ch, true);
+//				if(tmp != ch) {
+//					map.put(tmp, false);
+//					if(!map.get(ch)) {
+//						isGroup = false;
+//						break;
+//					}
+//				}
+//				tmp = ch;
+//			}
+//			if(isGroup) result++;
+//		}
+//		
+//		System.out.println(result);
+//		
+//		br.close();
 	}
 
 }
