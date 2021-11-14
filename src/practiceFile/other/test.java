357d6a4cd97d18c7c49cb42b1264d3fff42f445f
bslkuye <jdh960401@naver.com>
2021-11-14 18:55:23 +0900

    t

--- src/practiceFile/other/test.java
+++ src/practiceFile/other/test.java
@@ -140,7 +140,7 @@ public class test {
 
 		finder[1][1] = 3;
 		int stack = 1;			
-		while(finder[gridLength-2][gridLength-2] == 1 && stack < 1000) {
+		while(finder[gridLength-2][gridLength-2] == 1) {
 			stack++;
 			for(int i = 1; i < gridLength-1; i++) {
 				for(int j = 1; j < gridLength-1; j++) {
@@ -153,7 +153,7 @@ public class test {
 				}
 			}
 		}
-		System.out.println("rFind3" + stack);
+	
 
 		int a = gridLength - 2;
 		int b = gridLength - 2;
