f92f556347a5c5b26e4dc91e4dda68688602ca92
bslkuye <jdh960401@naver.com>
2021-11-21 20:30:08 +0900

    t

---
+++ src/practiceFile/other/programmers/failureRate.java
@@ -0,0 +1,13 @@
+package practiceFile.other.programmers;
+
+class failure {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        return answer;
+    }
+}
+
+public class failureRate {
+
+}
+//https://programmers.co.kr/learn/courses/30/lessons/42889
