fab36516cc1ff17daafa1884e5ca1e097dd31783
bslkuye <jdh960401@naver.com>
2021-11-26 15:13:29 +0900

    백준_lv8

---
+++ src/level8/no2869/Main.java
@@ -0,0 +1,31 @@
+package level8.no2869;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
+		double result = ((double)v-(double)b)/((double)a-(double)b);
+		System.out.println((int)Math.ceil(result));
+		
+		br.close();
+	}
+
+}
+
+//		for(int i = 1;;i++) {
+//			v -= a;
+//			if(v <= 0) {
+//				System.out.println(i);
+//				break;
+//			}
+//			v += b;
+//		}
