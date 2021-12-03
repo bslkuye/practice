66e38c556d34959675b750e60f54c1e3f37985d3
bslkuye <jdh960401@naver.com>
2021-12-03 22:04:06 +0900

    백준_lv9

--- src/baekjoon/level9/no9020/Main.java
+++ src/baekjoon/level9/no9020/Main.java
@@ -1,8 +1,6 @@
 package baekjoon.level9.no9020;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -10,9 +8,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> resultList = new ArrayList<>();
         int caseNum = Integer.parseInt(br.readLine());
         int max = 1;
+
         for(int i = 0; i < caseNum; i++) {
             int num = Integer.parseInt(br.readLine());
             list.add(num);
@@ -25,23 +26,28 @@ public class Main {
         Arrays.fill(isPrime, true);
         isPrime[0] = isPrime[1] = false;
 
-        for(int i=2;i*i<=max;i++) {
+        for(int i = 2; i <= max; i++) {
             for(int j=i*i;j<=max;j+=i) {
                 isPrime[j] = false;
             }
         }
-
-        for(int i=0;i<list.size();i++) {
-            int cnt = 0;
-            String result = "";
-            for(int j=2+1;j<=list.get(i)/2;j++) {
-                if(isPrime[j]) {
-                    cnt++;
-                }
-            }
-            System.out.println(cnt);
+        for(int i = 0; i < max; i++){
+            if(isPrime[i]) resultList.add(i);
         }
 
+        for(int i=0;i<list.size();i++) {
+            String result = "2 2";
+            for(int j = 0; resultList.get(j) <= list.get(i)/2; j++) {
+                for(int k = j; k < resultList.size(); k++){
+                    if(resultList.get(j) + resultList.get(k) == list.get(i)){
+                        result = Integer.toString(resultList.get(j)) + " " + Integer.toString(resultList.get(k));
+                    }
+                }
+            }
+            bw.write(result + "\n");
+        }
+        bw.flush();
+        bw.close();
         br.close();
     }
 
