124b22815915500a687839e9cb9c91575a027970
bslkuye <jdh960401@naver.com>
2022-01-04 20:42:29 +0900

    백준_lv14

--- src/baekjoon/level14/no9663/Main.java
+++ src/baekjoon/level14/no9663/Main.java
@@ -3,14 +3,21 @@ package baekjoon.level14.no9663;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     static int N;
     static int resultCount = 0;
+    static int[] arr;
+    static boolean[] check;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        check = new boolean[N];
+        Arrays.fill(check,false);
+
         if(N > 3){
             makeArr(N);
             System.out.println(resultCount);
@@ -21,37 +28,39 @@ public class Main {
         br.close();
     }
 
-    static int[] arr = new int[N];
 
     public static void makeArr(int n){
         if(n != 1){
             for(int i = 0; i < N; i++) {
-                arr[N - n] = i;
-                makeArr(n - 1);
+                if(check[i] == false){
+                    check[i] = true;
+                    arr[n - 1] = i;
+                    makeArr(n - 1);
+                    check[i] = false;
+                }
             }
         }else{
             for(int i = 0; i < N; i++) {
-                arr[N - n] = i;
-                if(checkArr(arr)){
-                    resultCount++;
+                if(check[i] == false){
+                    arr[n - 1] = i;
+                    if (checkArr(arr)) {
+                        resultCount++;
+                    }
                 }
             }
         }
     }
 
     public static boolean checkArr(int[] Arr){
-        boolean check = true;
         for(int i = 0; i < N - 1; i++){
             for(int j = i + 1; j < N; j++){
-                if(Arr[i] == Arr[j]){
-                    check = false;
-                }
                 if(Arr[i] - Arr[j] == i-j || Arr[i] - Arr[j] == j-i){
-                    check = false;
+                    return false;
                 }
             }
         }
-        return check;
+        return true;
     }
 
 }
+//https://www.acmicpc.net/problem/9663
