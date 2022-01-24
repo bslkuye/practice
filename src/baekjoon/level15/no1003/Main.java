d8f219ce7bcb9432cb725a95a1bc62359d75421a
bslkuye <jdh960401@naver.com>
2022-01-24 22:11:03 +0900

    백준_lv14

---
+++ src/baekjoon/level15/no1003/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.level15.no1003;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+//    int fibonacci(int n) {
+//        if (n == 0) {
+//            printf("0");
+//            return 0;
+//        } else if (n == 1) {
+//            printf("1");
+//            return 1;
+//        } else {
+//            return fibonacci(n?1) + fibonacci(n?2);
+//        }
+//    }
+}
+//https://www.acmicpc.net/problem/1003
