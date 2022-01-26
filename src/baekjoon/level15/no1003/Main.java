77bcce90815acd1a56a194bba18093f443efe305
bslkuye <jdh960401@naver.com>
2022-01-26 19:46:35 +0900

    백준_lv15

--- src/baekjoon/level15/no1003/Main.java
+++ src/baekjoon/level15/no1003/Main.java
@@ -1,15 +1,17 @@
 package baekjoon.level15.no1003;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static int countZero = 0;
     public static int countOne = 0;
+    public static int[] fibArr = new int[41];
 
     public static void main(String[] args)throws IOException{
+        Arrays.fill(fibArr,-1);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         int N = 0;
@@ -23,16 +25,20 @@ public class Main {
     }
 
     public static int fibonacci(int n){
-        if (n == 0) {
-            countZero++;
-            return 0;
-        } else if (n == 1) {
-            countOne++;
-            return 1;
-        } else {
-            return fibonacci(n-1) + fibonacci(n-2);
-        }
-    }
+        if(fibArr[n] == -1){
+            if (n == 0) {
+                countZero++;
+                return 0;
+            } else if (n == 1) {
+                countOne++;
+                return 1;
+            } else {
+                fibArr[n] = fibonacci(n-1) + fibonacci(n-2);
+                return fibArr[n];
+            }
+        }else return fibArr[n];
 
+
+    }
 }
 //https://www.acmicpc.net/problem/1003
