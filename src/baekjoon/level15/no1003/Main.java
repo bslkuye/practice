13e2880ca587230c138d20008904ae77c5d829dc
bslkuye <jdh960401@naver.com>
2022-01-27 23:16:39 +0900

    t

--- src/baekjoon/level15/no1003/Main.java
+++ src/baekjoon/level15/no1003/Main.java
@@ -42,3 +42,37 @@ public class Main {
     }
 }
 //https://www.acmicpc.net/problem/1003
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int[][] numArr = new int[41][2];
+		numArr[0][0] = 1;
+		numArr[0][1] = 0;
+		numArr[1][0] = 0;
+		numArr[1][1] = 1;
+
+		for(int i=2;i<41;i++) {
+			numArr[i][0] = numArr[i-1][0] + numArr[i-2][0];
+			numArr[i][1] = numArr[i-1][1] + numArr[i-2][1];
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<T;i++) {
+			int N = Integer.parseInt(br.readLine());
+			sb.append(numArr[N][0]).append(" ").append(numArr[N][1]).append("\n");
+		}
+
+		System.out.println(sb);
+
+		br.close();
+	}
+
+}
+ */
