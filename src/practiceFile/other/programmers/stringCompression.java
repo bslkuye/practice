eb58238e239dc72f694699f017cbbd250b00596d
bslkuye <jdh960401@naver.com>
2022-02-21 20:21:41 +0900

    문자열 압축

--- src/practiceFile/other/programmers/stringCompression.java
+++ src/practiceFile/other/programmers/stringCompression.java
@@ -8,33 +8,57 @@ public class stringCompression {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        String[] result = new String[input.length()/2];
-        for(int cut = 2; cut <= input.length()/2;cut++){
-            String[] cutInput = new String[input.length()/cut];
-            int count = 0;
-            int cutCount = 0;
-            while(count != input.length()){
-                for(int i = 0; i < cut; i++){
-                    cutInput[cutCount] += input.charAt(count);
+        String[] result = new String[input.length()/2 + 1];
+        int count = 0;
+        for(int i = 0; i < input.length()/2 + 1; i++){ //i = 자르는 갯수
+            for(int j = 0; j < input.length(); j = j + i + 1){ //j 로 input 문자열 반복
+                if(input.substring(j, j+i) == input.substring(j+i+1, j+2*i+1)){
                     count++;
-                }
-                cutCount++;
-            }
-            count = 1;
-            String check = cutInput[0];
-            for(int i = 1; i < cutInput.length; i++){
-                if(check == cutInput[i]){
-                    count++;
-                }else if(cutInput[i] == null || check != cutInput[i]){
-                    if(count != 1) result[cut] += Integer.toString(count);
-                    result[cut] += check;
-                    count = 1;
-                    check = cutInput[i];
+                }else if(input.substring(j, j+i) != input.substring(j+i+1, j+2*i+1)){
+                    if(count != 0){
+                        result[i] += count + input.substring(j, j+i);
+                    }else {
+                        result[i] += input.substring(j, j+i);
+                    }
                 }
 
             }
 
         }
+        int min = result[0].length();
+        for(int i = 0; i < input.length()+1; i++){
+            if(min > result[0].length()){
+                min = result[0].length();
+            }
+        }
+        System.out.println(min);
+
+//        for(int cut = 1; cut <= input.length()/2;cut++){
+//            String[] cutInput = new String[input.length()/cut];
+//            int count = 0;
+//            int cutCount = 0;
+//            while(count != input.length()){
+//                for(int i = 0; i < cut; i++){
+//                    cutInput[cutCount] += input.charAt(count);
+//                    count++;
+//                }
+//                cutCount++;
+//            }
+//            count = 1;
+//            String check = cutInput[0];
+//            for(int i = 1; i < cutInput.length; i++){
+//                if(check == cutInput[i]){
+//                    count++;
+//                }else if(cutInput[i] == null || check != cutInput[i]){
+//                    if(count != 1) result[cut] += Integer.toString(count);
+//                    result[cut] += check;
+//                    count = 1;
+//                    check = cutInput[i];
+//                }
+//
+//            }
+//
+//        }
         br.close();
     }
 }
