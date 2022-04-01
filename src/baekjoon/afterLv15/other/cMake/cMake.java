9351a9d312dd1f11336d62ffda838d6c83822b74
bslkuye <jdh960401@naver.com>
2022-04-01 17:01:27 +0900

    백준lv15

---
+++ src/baekjoon/afterLv15/other/cMake/cMake.java
@@ -0,0 +1,28 @@
+package baekjoon.afterLv15.other.cMake;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class cMake {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+
+        while(true){
+            input = br.readLine();
+            switch (input){
+                case "make":
+                    System.out.print("input name, personality, job : ");
+                    StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+            }
+
+        }
+    }
+
+
+
+}
---
+++ src/baekjoon/afterLv15/other/cMake/cMakeClass.java
@@ -0,0 +1,30 @@
+package baekjoon.afterLv15.other.cMake;
+
+public class cMakeClass {
+
+    public class Human{
+        public String name;
+        public String personality;
+        public String job;
+
+//        public Human(){
+//            this("noName","noPersonality","noJob");
+//        }
+//
+//        public Human(String name, String personality, String job){
+//            this.job = job;
+//            this.personality = personality;
+//            this.name = name;
+//        }
+//
+
+//        public void firstSet(String name, String personality, String job){
+//            this.name = name;
+//            this.personality = personality;
+//            this.job = job;
+//        }
+
+
+
+    }
+}
