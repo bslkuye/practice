3753ddd6145643bb6496ae9c826ce03519e02af3
정도헌 <jdh960401@naver.com>
2022-04-13 20:18:44 +0900

    other

---
+++ src/baekjoon/afterLv15/other/cMake/MapAndMove.java
@@ -0,0 +1,30 @@
+package baekjoon.afterLv15.other.cMake;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MapAndMove {
+    public static int N;
+    public static int[][] mapArr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        mapArr = new int[N][N];
+
+        br.close();
+    }
+
+    public static void mapPrint(){
+        System.out.println("print!");
+
+        for(int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                System.out.print(mapArr[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
